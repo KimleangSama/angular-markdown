adf1db3031042a906784ac69bc0b0a8dbfc35cf3
Kimleang <56532167+KimleangSama@users.noreply.github.com>
2020-12-02 09:16:37 +0700

    Complete MarkDown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, VERSION} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'markdown-to-html';
+  angularVersion = VERSION.full;
+  ngxMarkdownVersion = '11.0.0';
+
+  markdown = `
+    ## Markdown __rulez__!
+    ---
+
+    ### Syntax highlight
+    \`\`\`typescript
+    const language = 'typescript';
+    \`\`\`
+
+    ### Lists
+    1. Ordered list
+    2. Another bullet point
+      - Unordered list
+      - Another unordered bullet point
+
+    ### Blockquote
+    > Blockquote to the max
+  `;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {MarkdownModule} from 'ngx-markdown';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
